7a70f6f6718c7201bba227779e61de568c70ba1a
Edward Kearn <ejkearn@gmail.com>
2018-05-18 14:39:52 -0600

    posts (story) router updated

--- server/serverAssets/routes/posts.js
+++ server/serverAssets/routes/posts.js
@@ -12,7 +12,40 @@ router.get('/api/posts', (req,res,next) =>{
   })
 })
 
-//add post
+router.get('/api/posts/:id',(req, res, next) =>{
+  Posts.findById(req.params.id)
+  .then(post=>{
+    res.status(200).send(post)
+  })
+  .catch(err => {
+    res.status(400).send(err)
+  })
+})
+
+//add post  ********* change below
+//create the post
+router.post('/api/posts', (req, res, next) =>{
+  var story = req.body
+  Posts.create(story)
+  .then(newStory =>{
+    res.status(200).send(newStory)
+  })
+  .catch(err =>{
+    res.status(400).send(err)
+  })
+})
+
+//delete user
+router.delete('/api/posts/:id', (req, res, next) =>{
+  Posts.findByIdAndRemove(req.params.id)
+    .then(data=>{
+      res.send("Successfully Deleted")
+    })
+    .catch(err =>{
+      res.status(400).send(err)
+    })
+})
+
 
 module.exports = {
   router
