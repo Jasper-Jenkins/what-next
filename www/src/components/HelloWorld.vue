6dd4ea7028374531b6247d853a6df0c0f1bc534f
Jasper Jenkins <ownyourfate@yahoo.com>
2018-05-18 15:56:11 -0600

    sort of working

--- www/src/components/HelloWorld.vue
+++ www/src/components/HelloWorld.vue
@@ -1,113 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="hello">  {{newUser.userName}}{{newUser.id}} <!--{{users.name}}-->
+    <div v-if="!newUser.id">
+      <!--vif="function to handle userid check"-->
+      <form  v-on:submit.prevent="addUser">
+          <input type="text" name="name" placeholder="please enter name" v-model="newUser.userName">
+          <button type='submit'>Submit</button>
+      </form> 
+    </div>
+    <div v-else>
+      <div>
+        <form v-on:submit.prevent="addPost">
+            <input type="string" name="title" placeholder="title" v-model="post.title">
+            <input type="string" name="body" placeholder="Kick ass story" v-model="post.body">
+            <button type='submit'>Submit</button>
+        </form>
+      </div>
+    
+      <div>{{msg}}</div>
+   
+      <div class="post" v-for="post in posts" :key='post._id'>
+        
+          <img :src="post.imgUrl">   
+          <h1>Title: {{post.title}}</h1>
+          <p>body: {{post.body}}</p>
+          <h6>Author: {{post.userId}}</h6>
+          <!-- <button @click="addComment">Add comment</button>
+          <button @click="deleteHouse(house)">Sold!</button>
+          -->
+      </div> 
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  mounted() {
+    this.$store.dispatch("getPosts");
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Tell us your story",
+      post: {
+        title: "",
+        userId: "",
+        body: "",
+        votes: 0,
+        imgUrl: "https://placehold.it/200x200"
+      },
+      newUser:{
+        userName: 'Jack',
+        id: 123,
+      }
+    };
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+    // users() {
+    //   return this.$store.state.users;
+    // },
+    user(){
+      return this.$store.state.user;
+    }
+  },
+  methods: {
+    // checkUser(name){
+    //   this.$store.dispatch("checkUser",this.users)
+
+    // },
+    addPost() {
+      this.$store.dispatch("addPost", this.post);
+    },
+    addUser(){
+      this.$store.dispatch("addUser", this.newUser);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
